a73b2ae58802262a3a7c487f1220d989b4c5af88
Monoj5212 <monoj5212@gmail.com>
2025-07-21 22:37:02 +0530

    Add files via upload

---
+++ Assignment2_Task1.py
@@ -0,0 +1,11 @@
+'''
+Write a Python program that:
+1. 	Takes an integer input from the user.
+2. 	Checks whether the number is even or odd using an if-else statement.
+'''
+
+a=int(input("Enter a Number to Check Odd or Even:"))
+if a%2==0:
+    print(a,"is Even number.")
+else:
+    print(a,"is Odd number.")
---
+++ Assignment2_Task2.py
@@ -0,0 +1,10 @@
+"""
+Write a Python program that:
+1.   Uses a for loop to iterate over numbers from 1 to 50.
+2.   Calculates the sum of all integers in this range.
+"""
+
+add=0
+for a in range(1,51):
+    add=add+a
+print("The sum of numbers 1 to 50 is:",add)
